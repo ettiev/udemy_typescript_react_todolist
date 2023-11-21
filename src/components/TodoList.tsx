71232180fe7afda633ddcc14824c5ccc63e6df56
ettiev <126219891+ettiev@users.noreply.github.com>
2023-11-21 16:23:29 +0200

    Finished Udemy project - TS and React
    A finished Udemy project using TypeScript in React

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./TodoList.css";
+
+interface TodoListProps {
+    items: {id: string, text: string}[];
+    onDeleteTodo: (id: string) => void;
+};
+
+const TodoList: React.FC<TodoListProps> = props => {
+    
+    return (<ul>
+        {props.items.map(item => (
+        <li key={item.id}>
+            <span>{item.text}</span>
+            <button onClick={props.onDeleteTodo.bind(null, item.id)}>Delete</button>
+        </li>))}
+    </ul>
+    );
+};
+
+export default TodoList;
